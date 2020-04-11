859e5d08f69d6da68a57a7392e8959dee673d5ac
tiyarocks <51966091+tiyarocks@users.noreply.github.com>
2020-04-11 13:59:43 +0530

    Add files via upload
    integrating redux and react

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {selectSong} from '../actions'
+
     //default  //named
 class SongList extends Component {
+    renderList(){
+        return this.props.songs.map((song)=>{
+            return(
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button 
+                            className="ui button primary"
+                            onClick={()=>this.props.selectSong(song)}
+                        >
+                        SELECT
+                        </button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            )
+        })
+    }
     render() {
         //this.props==={songs:state.song}
         console.log(this.props)
         return (
-            <div>
-              SongList  
+            <div className="ui divided list">
+              {this.renderList()}  
             </div>
         )
     }
 }
 const mapStateToProps=(state=>{
-
+    console.log(state)
     return{
         songs:state.songs
     }
 })
-export default connect(mapStateToProps)(SongList)
+export default connect(mapStateToProps,{selectSong})(SongList)
